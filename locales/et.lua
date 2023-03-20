36bbc9d1a29eda7d563796bb7aeb57f743c563dd
Walls <blumvaido@gmail.com>
2023-03-21 01:05:27 +0200

    Create et.lua
    Add estonian locales for the resource.

---
+++ locales/et.lua
@@ -0,0 +1,25 @@
+Locales['et'] = { 
+
+    -- want to use your own, custom coloring? Use:
+    -- https://wiki.rage.mp/index.php?title=Fonts_and_Colors
+
+    ['lottery_name'] = 'Kraabi ja Võida', -- name in result of ticket notification
+    ['lottery_subject'] = 'Kiirloosi kaardi tulemus', -- subject in result of ticket notification
+    ['active_cooldown'] = 'Sa juba kraapisid üht kaarti hiljuti, sa pead ootama : %s sekundit veel!', -- %s <cooldown in seconds>
+    ['used_scratchticket'] = 'Edu sulle!.',
+    ['currency'] = '$',
+    ['scratch_lost'] = 'Sa kaotasid :(', -- default UPPERCASE
+    ['scratch_won'] = 'Sa võitsid!',
+
+    -- Webhooks
+    -- Want to use Discords markdown text? Use:
+    -- https://support.discord.com/hc/en-us/articles/210298617-Markdown-Text-101-Chat-Formatting-Bold-Italic-Underline-
+
+    ['webhook_resourceName'] = "[ %s ]", -- %s <resource name>
+    ['webhook_identifier'] = 'Identifier', -- current player his/her identifier
+    ['webhook_winMessage_cash'] = '**%s** võitis $%s kasutades kiirloosi piletit!', -- (1) %s <name> (2) %s <cash amount>
+    ['webhook_winMessage_item'] = '**%s** võitis %sx %s kasutades kiirloosi piletit!', -- (1) %s <name> (2) %s <amount> (3) <item name>
+    ['webhook_loseMessage'] = '**%s** kaotas kasutades kiirloosi piletit.', -- %s <name>
+    ['webhook_possibleCheatingAttempt'] = '**%s** triggeris *võimaliku* häki tuvastuse.', -- %s <name>
+    ['webhook_message'] = 'Message' -- cheating attempt message header
+}
